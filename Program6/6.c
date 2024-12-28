0f0fabbd2f65e1c9e63a8d18aeba71c84e770de1
Rohith Gowda M <rohithgowda150503@gmail.com>
2024-12-28 13:00:12 +0530

    akash

--- Program6/6.c
+++ Program6/6.c
@@ -1,46 +1,58 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cctype>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <omp.h>
 
 #define SIZE 5
-#define FILE "words.txt"
+#define FILE_NAME "words.txt"
 
-using namespace std;
+char *words[SIZE] = {"lorem", "ipsum", "dolo", "sit", "amet"};
+int counts[SIZE] = {0};
 
-string words[SIZE] = {"lorem", "ipsum", "dolo", "sit", "amet"};
-int counts[SIZE] = {};
-
-void lower(string &str) {
-	for (int i = 0; i < str.size(); i++)
-		str[i] = tolower(str[i]);
+void to_lower(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = tolower(str[i]);
+    }
 }
 
-int getCount(string fileName, string key) {
-	lower(key);
-	ifstream file(fileName);
-	string word;
-	int count = 0;
-	while (file >> word) {
-		lower(word);
-		if(key == word) count++;
-	}
-	return count;
+int get_count(const char *file_name, const char *key) {
+    char lower_key[100];
+    strcpy(lower_key, key);
+    to_lower(lower_key);
+
+    FILE *file = fopen(file_name, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file: %s\n", file_name);
+        return 0;
+    }
+
+    char word[100];
+    int count = 0;
+    while (fscanf(file, "%99s", word) == 1) {
+        to_lower(word);
+        if (strcmp(lower_key, word) == 0) {
+            count++;
+        }
+    }
+    fclose(file);
+    return count;
 }
 
 int main() {
-	double t = omp_get_wtime();
+    double t = omp_get_wtime();
 
-	#pragma omp parallel for // num_threads(1/2/4/8)
-	for (int i = 0; i < SIZE; i++) {
-		counts[i] = getCount(FILE, words[i]);
-	}
+    #pragma omp parallel for
+    for (int i = 0; i < SIZE; i++) {
+        counts[i] = get_count(FILE_NAME, words[i]);
+    }
 
-	t = omp_get_wtime() - t;
-	cout << "Time: " << t << endl;
+    t = omp_get_wtime() - t;
+    printf("Time: %f seconds\n", t);
 
-	for (int i = 0; i < SIZE; i++) {
-		cout << words[i] << " " << counts[i] << endl;
-	}
+    for (int i = 0; i < SIZE; i++) {
+        printf("%s %d\n", words[i], counts[i]);
+    }
+
+    return 0;
 }
